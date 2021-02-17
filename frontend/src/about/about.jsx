771c5cf749385bbb05b850e93c79fa09a2441c8a
JoseLucasapp <josluc2016@gmail.com>
2021-02-17 19:18:58 -0300

    update

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import PageHeader from '../template/pageHeader';
+
+export default (props)=>(
+    <div>
+        <PageHeader name="Sobre" small="Nós" />
+
+        <h2>Quem somos?</h2>
+        <p>xjasjxasxakjxksjxljidjwiexdis</p>
+        <h2>Contato</h2>
+        <p>90909090909099</p>
+    </div>
+)
---
+++ frontend/src/todo/todo.jsx
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+import PageHeader from '../template/pageHeader';
+import TodoForm from './todoForm';
+import TodoList from './todoList';
+
+export default class Todo extends Component{
+    render(){
+        return(
+            <div>
+                <PageHeader name="Tarefas" small="Cadastro" />
+
+                <TodoForm />
+                <TodoList />
+            </div>
+        )
+    }
+}
