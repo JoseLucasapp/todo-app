9d814b863f967e61c2ae79e481609e328a0f9ce6
JoseLucasapp <josluc2016@gmail.com>
2021-02-18 21:58:23 -0300

    update frontend

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -5,12 +5,20 @@ import TodoForm from './todoForm';
 import TodoList from './todoList';
 
 export default class Todo extends Component{
+    constructor(){
+        super()
+        this.handleAdd = this.handleAdd.bind(this);
+    }
+
+    handleAdd(){
+    }
+
     render(){
         return(
             <div>
                 <PageHeader name="Tarefas" small="Cadastro" />
 
-                <TodoForm />
+                <TodoForm handleAdd={this.handleAdd} />
                 <TodoList />
             </div>
         )
--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
+import Grid from '../template/grid';
+import IconButton from '../template/iconButton';
 
 export default (props)=>(
-    <div>Form</div>
+    <div role="form" className="todoForm">
+        <Grid cols={"12 9 10"}>
+            <input id="description" className="form-control" placeholder="Adicione tarefa" />
+        </Grid>
+        <Grid cols={"12 3 10"}>
+            <IconButton onClick={props.handleAdd} style="primary" icon="plus"></IconButton>
+        </Grid>
+    </div>
 )
